1155a01a6b7a2ab5e2e08cbed3d9cfe255fbbdbf
Jan Kuehle <jkuehle90@gmail.com>
2017-06-25 17:39:03 +0200

    Add build script.

---
+++ build.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+/* eslint-disable no-console */
+
+const del = require('del');
+const cpy = require('cpy');
+
+async function build() {
+    await del('dist/');
+    await cpy('node_modules/redux/dist/redux.min.js', 'dist/node_modules/redux/dist/');
+    await cpy('src/bower_components/webcomponentsjs/webcomponents-*.js', 'dist/bower_components/webcomponentsjs/');
+    await cpy('src/elements/mixins/*.js', 'dist/elements/mixins/');
+    await cpy('src/elements/*.js', 'dist/elements/');
+    await cpy('src/music/*.mp3', 'dist/music/');
+    await cpy('src/utils/reducers/*.js', 'dist/utils/reducers/');
+    await cpy('src/utils/*.js', 'dist/utils/');
+    await cpy('src/index.html', 'dist/');
+    await cpy('src/style.css', 'dist/');
+}
+
+build().catch(console.error);
